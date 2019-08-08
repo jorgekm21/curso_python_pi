7bff6b9b36e55810213be87b1cd64ef68f504b4b
Jorge Kassabji <jorgekm21@gmail.com>
2019-08-08 15:10:49 -0400

    Clase 27
    Programacion Orientada a Objetos

--- poo_1.py
+++ poo_1.py
@@ -1,24 +1,43 @@
 class Coche(): #Creacion de una clase
-    largoChasis=250 #Parametros iniciales que siempre estaran en los objetos creados
-    anchoChasis=120
-    ruedas=4
-    enmarcha=False #Indica que el coche esta detenido
 
-    def arrancar(self): #Creacion de metodo o comportamiendo del objeto
-        self.enmarcha=True
-        # pass #Permite pasar de largo sin generar error
+    def __init__(self): #Metodo de constructor, define las condiciones iniciales de los objetos
+    # Cuando se crea un objeto se ejecuta automatica este metodo
 
-    def estado(self):
-        if(self.enmarcha):
+        self.__largoChasis=250 #Parametros iniciales que siempre estaran en los objetos creados
+        self.__anchoChasis=120
+        self.__ruedas=4 #Con 2 guiones bajos, haces que la variable no sea accesible desde afuera de la clase, pero si desde adentro
+        # Se conoce como encapsular una variable
+        self.__enmarcha=False #Indica que el coche esta detenido
+
+    def arrancar(self,arrancamos): #Creacion de metodo o comportamiendo del objeto
+
+        self.__enmarcha=arrancamos
+
+        if(self.__enmarcha):
             return "El coche esta en marcha"
         else:
             return "El coche esta parado"
+        # pass #Permite pasar de largo sin generar error
+
+    def estado(self):
+        print("El coche tiene ", self.__ruedas, " ruedas. Un ancho de ", self.__anchoChasis, " y un largo de ", self.__largoChasis)
+
+ #---------------------------------------------------------------------------------------------------------------------------       
 
 miCoche=Coche() #Instanciar una clase, Ejemplarizar
 
-print("El largo del coche es: ",miCoche.largoChasis)
-print("El coche tiene ", miCoche.ruedas, " ruedas")
-miCoche.arrancar() #Ejecuta el contenido dentro del metodo arrancar
-print(miCoche.estado())
+print(miCoche.arrancar(True)) #Ejecuta el contenido dentro del metodo arrancar
+
+miCoche.estado()
 
 # Clase coche posee 4 propiedades y 2 comportamientos
+
+print("----------------A continuacion creamos el segundo objeto---------------------") #Separador para guiar
+
+miCoche2=Coche() #Segundo objeto creado
+
+print(miCoche2.arrancar(False))
+
+miCoche2.ruedas=2
+
+miCoche2.estado() #Cuando se ejecuta se ve la diferencia al no haber ejecutado el metodo arrancar
