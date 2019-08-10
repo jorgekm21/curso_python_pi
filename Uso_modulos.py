f0f4dc2c5bbfd1af2d22801df437614f5ce68af7
Jorge Kassabji <jorgekm21@gmail.com>
2019-08-10 15:27:23 -0400

    Clase 35 - Paquetes

---
+++ Uso_modulos.py
@@ -0,0 +1,7 @@
+""" from calculos.calculos_generales import * #Llama el paquete dentro de la carpeta calculos
+
+redondear(4.5) """
+
+from calculos.redondeo_potencia.redondeaYpotencia import *
+
+potencia(4,5)
