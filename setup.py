409bc4d15f0abdd6ab1b0876942859672dea8b80
Jorge Kassabji <jorgekm21@gmail.com>
2019-08-11 10:03:44 -0400

    Clase 36 - Paquetes Distribuibles

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    
+    name="paquetecalculos",
+    version="1.0",
+    description="Paquete de redondeo y potencia",
+    author="Jorge Kassabji",
+    author_email="jorgekm21@gmail.com",
+    url="www.pcnsuministros.com.ve",
+    packages=["calculos","calculos.rendondeo_potencia"]
+
+)
+
+#En el CMD se escribe el comando "python nombrearchivo.py sdist"
