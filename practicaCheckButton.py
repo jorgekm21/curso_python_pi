3defb35ce39614d3ace15e89ef27ebd877fbcdc1
Jorge Kassabji <jorgekm21@gmail.com>
2020-03-20 08:31:48 -0400

    clase 51 - Interfaces Graficas X

---
+++ practicaCheckButton.py
@@ -0,0 +1,40 @@
+from tkinter import *
+
+root=Tk()
+
+root.title("Ejemplo")
+
+playa=IntVar()
+montana=IntVar()
+turismoRural=IntVar()
+
+def opcionesViaje():
+
+    opcionEscogida=""
+
+    if(playa.get()==1):
+        opcionEscogida+=" playa"
+    if(montana.get()==1):
+        opcionEscogida+=" montaña"
+    if(turismoRural.get()==1):
+        opcionEscogida+=" turismo Rural"
+
+    textoFinal.config(text=opcionEscogida)
+
+foto = PhotoImage(file="logo.png")
+Label(root, image=foto).pack()
+
+frame=Frame(root)
+frame.pack()
+
+Label(frame, text="Elige destinos", width=50).pack()
+
+Checkbutton(frame, text="Playa", variable=playa, onvalue=1, offvalue=0, command=opcionesViaje).pack()
+Checkbutton(frame, text="Montaña", variable=montana, onvalue=1, offvalue=0, command=opcionesViaje).pack()
+Checkbutton(frame, text="Turismo Rural", variable=turismoRural, onvalue=1, offvalue=0, command=opcionesViaje).pack()
+
+textoFinal = Label(frame)
+textoFinal.pack()
+
+
+root.mainloop()
